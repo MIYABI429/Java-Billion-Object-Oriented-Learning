4909bdc354ad48dd40e25feea7c4964a261b3e88
三好勇輝 <miyabi04299@gmail.com>
2024-08-12 15:01:17 +0900

    a

--- src/lesson2/Lesson2_2.java
+++ src/lesson2/Lesson2_2.java
@@ -1,5 +1,10 @@
 package lesson2;
 
 public class Lesson2_2 {
+	public static final int STONE = 0;		//グー
+	public static final int SCISSORS = 1;	//チョキ
+	public static final int PAPER = ２;		//パー
+		public static final int         sdddPAER = ２;		//パー
 
 }
+
