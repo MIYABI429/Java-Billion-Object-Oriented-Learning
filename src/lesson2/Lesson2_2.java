9d3a21d4090dbf368da5946e6db2920bcef96b00
三好勇輝 <miyabi04299@gmail.com>
2024-08-12 15:36:17 +0900

    リスト2‐3開始

--- src/lesson2/Lesson2_2.java
+++ src/lesson2/Lesson2_2.java
@@ -4,7 +4,5 @@ public class Lesson2_2 {
 	public static final int STONE = 0;		//グー
 	public static final int SCISSORS = 1;	//チョキ
 	public static final int PAPER = ２;		//パー
-		public static final int         sdddPAER = ２;		//パー
-
 }
 
